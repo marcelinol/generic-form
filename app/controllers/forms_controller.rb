11f27726df72082ddf7d09707dce88a59d2ee864
Luciano Medeiros <marcelinolucianom@gmail.com>
2016-05-21 17:24:51 -0300

    wip

--- app/controllers/forms_controller.rb
+++ app/controllers/forms_controller.rb
@@ -7,5 +7,19 @@ class FormsController < ApplicationController
   end
 
   def create
+
+  end
+
+  def show
+    @form = sample
+  end
+
+  def sample
+    {
+      fields: [
+        { name: 'field 0', type: 'text' },
+        { name: 'field 1', type: 'text' }
+      ]
+    }.to_json
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   root 'forms#new'
 
   resources :forms, only: [:index, :new, :create]
+
+  get '/form', to: 'forms#show'
 end
